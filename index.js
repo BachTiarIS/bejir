6e1390e94686d47cbed5824c266402f2cb4a94cb
BachTiarIS <168549155+BachTiarIS@users.noreply.github.com>
2024-05-09 22:43:15 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+function search() {
+    let searchInput = document.getElementById('searchInput').value;
+    alert('Anda mencari: ' + searchInput);
+    // Lakukan tindakan pencarian sesuai kebutuhan
+}
